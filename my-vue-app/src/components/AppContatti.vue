a66ab768bf597a4893508f48a9d8f82fa09acc1e
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-05-31 17:48:01 +0100

    footer sezione pagamenti no style

--- my-vue-app/src/components/AppContatti.vue
+++ my-vue-app/src/components/AppContatti.vue
@@ -5,5 +5,25 @@
 </script>
 
 <template>
-    
+  <div class="container-fluid paymentBg d-flex flex-column">
+    <div class="align-items-center">
+      <h4 class="text-white">©Copyright 2012-2020 | Avada theme by Theme Fusion | All Rights reserved | Powered by WordPress</h4>
+    </div>
+
+    <!--img pagamenti-->
+    <div class="align-items-center">
+      <img class="immagine" src="../assets/images/payment_cards_footer.png">
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+  .paymentBg{
+    height: 7.5rem;
+    background-color: #1d1f22;
+  }
+  .immagine{
+    height: 50px;
+    width: 500px;
+  }
+</style>
