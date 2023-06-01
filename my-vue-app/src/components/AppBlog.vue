d643fe9a9c35ff6bbb5ee271766c5cc0169a93e2
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-06-01 16:38:28 +0100

    blog section

--- my-vue-app/src/components/AppBlog.vue
+++ my-vue-app/src/components/AppBlog.vue
@@ -1,8 +1,68 @@
 <script>
    export default {
-    name: "Blog"
+    name: "Blog",
+
+    data() {
+      return {
+         blogPost : [
+            {
+               img: "./src/assets/images/post_img_12-700x441.jpg",
+               titolo: "Aenean lobortis sapien enim viverra",
+               data: "September 9th, 2015 | ",
+               commenti: "0 Comments",
+               testo: "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla."
+            },
+            {
+               img: "./src/assets/images/post_img_11-700x441.jpg",
+               titolo: "Duis ac massa semper maximus",
+               data: "September 9th, 2015 | ",
+               commenti: "0 Comments",
+               testo: "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla."
+            },
+            {
+               img: "./src/assets/images/post_img_10-700x441.jpg",
+               titolo: "Nunc fermit nulla eu justo sem id",
+               data: "September 9th, 2015 | ",
+               commenti: "0 Comments",
+               testo: "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla."
+            }
+         ]
+      }
+    }
    }
 </script>
 
 <template>
+   <div class="container-fluid blogBox">
+      <div class="row h-30 justify-content-center align-items-center">
+         <div class="col-3"><hr class="mb-5" /></div>
+         <div class="col-auto text-center">
+            <h1>From Our Blog</h1>
+            <p>The latest Classic Shop news</p>
+         </div>
+         <div class="col-3"><hr class="mb-5"/></div>
+      </div>
+      <div class="row blogCard h-70 justify-content-center align-items-center">
+         <div class="col d-flex flex-wrap flex-column justify-content-center align-items-start p-0" v-for="post in blogPost" :key="post">
+            <img class="blogImg" :src="post.img" alt="">
+            <h5>{{ post.titolo }}</h5>
+            <small>{{ post.data }} {{ post.commenti }}</small>
+            <p>{{ post.testo }}</p>
+         </div>
+      </div>
+      
+   </div>
 </template>
+
+<style lang="scss">
+   .blogBox{
+      height: 720px;
+   }
+   .blogCard{
+      padding: 10px 170px;
+   }
+   .blogImg{
+      height: 315px;
+      width: 500px;
+   }
+</style>
